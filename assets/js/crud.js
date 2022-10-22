3ed94a351dc3c7f814c5202f984cda470f7f379f
Ossama kharbaq <ossamakharbaq4@gmail.com>
2022-10-22 18:06:18 +0100

    update function but needs to edit it more and fixe the syntaxe

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -20,6 +20,11 @@
 // gets all buttons those we need
         let submitBtn = document.getElementById('submit');
 
+
+// lets create a helpers 
+let mood = 'create'; 
+let index;
+
 // adding task function
 
 let tasks;
@@ -38,12 +43,23 @@ if(localStorage.myTasks != null){
                 date : taskDate.value,
                 description : taskDescription.value
         }
-        tasks.push(newTask);
+
+        if(mood === 'create'){
+                tasks.push(newTask);
+                modalFooter.innerHTML=`
+                <button class="btn text-black" aria-label="submit" id="submit"  data-bs-toggle="modal" >DONE</button>
+                `;
+                clearForm();
+        }else{
+                
+                tasks[index] = newTask;
+        }
+        
         // save in localStorage 
         localStorage.setItem('myTasks',JSON.stringify( tasks));
         console.log(tasks);
         
-        clearForm();
+       
         displayData();
 }
 // clear the Form for another task 
@@ -73,7 +89,7 @@ function displayData(){
                 todo.innerHTML += `<button class="border-0 w-100 rounded mb-2 shadow-sm d-flex text-start align-items-center" id="bContainer" style="background-color: #FAF7F0;">
                 <div class="editD">
                         <i onclick="deleteTask(${i})"  class="bi bi-trash mt-2" ></i>
-                        <i class="bi bi-pencil-square mt-1" href="#"></i>
+                        <i class="bi bi-pencil-square" onclick="fullForm(${i});" data-bs-toggle="modal" href="#modal-task" mt-1" href="#"></i>
                 </div>
                 <div class="" style="width: 10%;">
                     <i class="bi bi-exclamation fw-bold fs-15px"></i> 
@@ -142,9 +158,24 @@ function displayData(){
         }
     }
 
+    // get spans counter elements and put the count inside them
+
+    document.getElementById('to-do-tasks-count').innerHTML= tasksNumber[0];
+    document.getElementById('in-progress-tasks-count').innerHTML= tasksNumber[1];
+    document.getElementById('done-tasks-count').innerHTML= tasksNumber[2];
+
+    // set a button that make us clear the board
+    let deleteAll = document.getElementById('clear-all');
+    if(tasks.length > 0){
+        deleteAll.innerHTML =`<button type="button" onclick="deleteAllTasks()" class="btn bg-test"> ! CLEAR BOARD ! </button>`;
+    }else{
+        deleteAll.innerHTML ='';
+    }
+
 }
 displayData();
 
+      
 // delete function 
 
 function deleteTask(i){
@@ -152,4 +183,35 @@ function deleteTask(i){
         tasks.splice(i,1);
         localStorage.myTasks= JSON.stringify(tasks);
         displayData();
+}
+
+// the function that makes us clear the board 
+function deleteAllTasks(){
+        localStorage.clear();
+        tasks.splice(0);
+        displayData();
+        console.log("deleteAllFunction works");
+}
+
+let modalFooter = document.getElementById('modal-footer');
+function fullForm(i){
+        modalFooter.innerHTML=`
+        <button class="btn text-black" aria-label="submit" data-bs-toggle="modal" >Save Changes</button>
+        `;
+        taskTitle.value         = tasks[i].title;
+        taskType.value          = tasks[i].type;
+        taskPriority.value      = tasks[i].priority;
+        taskStatus.value        = tasks[i].status;
+        taskDate.value          = tasks[i].date;
+        taskDescription.value   = tasks[i].description;
+        // console.log('fullForm works');
+        mood = 'update';
+        index = i;
+}
+
+function updateTask(i){
+        console.log(i);
+        modalFooter.innerHTML=`
+        <button class="btn text-black" aria-label="submit" id="submit"  data-bs-toggle="modal" >DONE</button>
+        `;
 }
